5fb2c61e032c71bed2f2bd928c7b782205edc19e
JUNY0110 <98405970+JUNY0110@users.noreply.github.com>
2022-06-28 00:26:45 +0900

    Chore🧹: CustomTextEditor 삭제 [#10]
    MainView와 ModifyingInfo에 동일한 코드가 있어 충돌 우려.

--- Jikyeojulge/View/MainView.swift
+++ Jikyeojulge/View/MainView.swift
@@ -82,26 +82,3 @@ struct MainView_Previews: PreviewProvider {
         MainView()
     }
 }
-
-
-struct CustomTextEditor: View {
-    let placholder: String
-    @Binding var medicalRecord: String
-    
-    var body: some View {
-        
-        ZStack(alignment: .topLeading) {
-            TextEditor(text: $medicalRecord)
-                .frame(width: 302, height: 156)
-                .background(Rectangle()
-                    .stroke(Color.black.opacity(0.5)))
-                .padding(.horizontal, 24)
-                .foregroundColor(medicalRecord == "지병에 대해 적어주세요." ? .gray: .primary)
-                .onTapGesture {
-                    if medicalRecord == "지병에 대해 적어주세요." {
-                        medicalRecord = ""
-                    }
-                }
-        }
-    }
-}
