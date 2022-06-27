c9c7118f5143fe6a4e30593d216921e7379f6126
taek0622 <min4644@gmail.com>
2022-06-27 23:34:41 +0900

    Chore🧹: Modify Medicine Info Structure (#11)

--- Jikyeojulge/View/MedicineRecordView.swift
+++ Jikyeojulge/View/MedicineRecordView.swift
@@ -9,58 +9,110 @@ import SwiftUI
 
 struct MedicineInfo: View {
     var body: some View {
-        ZStack {
-            Color.white
-            HStack {
-                Image(systemName: "heart.fill")
-                    .resizable()
-                    .frame(width: 100, height: 100)
-                    .foregroundColor(Color.red)
-                VStack(alignment: .leading) {
-                    Text("이부프로펜")
-                        .font(.system(size: 20, weight: .bold))
-                    Text("-효과")
-                    Text("이 약은 약물중독, 자가중독에 사용합니다.")
-                }
+        HStack {
+            Image("DefaultMedicine")
+                .resizable()
+                .frame(width: 80, height: 80)
+                .cornerRadius(10)
+                .foregroundColor(Color.red)
+            
+            Spacer()
+                .frame(width: 16)
+            
+            VStack(alignment: .leading, spacing: 15) {
+                Text("이부프로펜")
+                    .font(.system(size: 16, weight: .bold))
+                
+                Text("이 약은 위산과다, 속쓰림, 위부불쾌감, 위부팽만감, 체함, 구역, 구토, 위통, 신트림에 사용합니다.")
+                    .font(.system(size: 14))
+                    .lineLimit(2)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .multilineTextAlignment(.leading)
             }
         }
-        .frame(maxWidth: .infinity)
-    }
-}
-
-struct MedicineInfo_Previews: PreviewProvider {
-    static var previews: some View {
-        MedicineInfo()
+        .padding(.vertical, 10)
+        .background(RoundedRectangle(cornerRadius: 10).fill(Color.white))
     }
 }
 
 struct MedicineRecordView: View {
-    @State private var tempText = ""
+    @State private var isShowingSheet = false
+    @State private var isShowingFullScreen = false
+    @State private var startDate = Date()
+    @State private var endDate = Date()
     
     var body: some View {
         ZStack(alignment: .top) {
-            Color.gray
+            Color.mainBlue
                 .ignoresSafeArea()
+            
             VStack {
                 HStack {
-                    TextField("placeholder", text: $tempText)
+                    DatePicker("시작 날짜", selection: $startDate, in:...Date(), displayedComponents: .date)
+                        .labelsHidden()
                     Text("~")
-                    TextField("placeholder", text: $tempText)
+                    DatePicker("끝 날짜", selection: $endDate, in:...Date(), displayedComponents: .date)
+                        .labelsHidden()
                     Button(action: {
                         
                     }, label: {
                         Text("확인")
+                            .foregroundColor(Color.white)
                     })
                     .padding(5)
-                    .background(RoundedRectangle(cornerRadius: 10))
+                    .background(RoundedRectangle(cornerRadius: 5).fill(Color.subBtnBlue))
                 }
-                MedicineInfo()
-                MedicineInfo()
+                .padding(.horizontal, 20)
+                .padding(.top, 15)
+                
+                List {
+                    NavigationLink(destination: { Text("A") }, label: {
+                        MedicineInfo()
+                    })
+                    NavigationLink(destination: { Text("B") }, label: {
+                        MedicineInfo()
+                    })
+                    NavigationLink(destination: { Text("C") }, label: {
+                        MedicineInfo()
+                    })
+                }
+                .onAppear { UITableView.appearance().backgroundColor = UIColor.clear
+                    UITableView.appearance().contentInset.top = -20
+                }
             }
-            .padding(.horizontal, 20)
         }
         .navigationTitle("진단서 및 복용약")
         .navigationBarTitleDisplayMode(.inline)
+        .toolbar {
+            ToolbarItem(content:  {
+                Button(action: {
+                    isShowingSheet = true
+                }, label: {
+                    Text("추가")
+                })
+                Divider()
+            })
+        }
+        .confirmationDialog("동작 선택", isPresented: $isShowingSheet, actions: {
+            Button(action: {
+                isShowingFullScreen = true
+            }, label: {
+                Text("조회하여 추가하기")
+            })
+            Button(action: {
+                
+            }, label: {
+                Text("사진 촬영하기")
+            })
+            Button(action: {
+                
+            }, label: {
+                Text("앨범에서 선택하기")
+            })
+        })
+        .fullScreenCover(isPresented: $isShowingFullScreen, content: {
+            MedicineSearchView()
+        })
     }
 }
 
