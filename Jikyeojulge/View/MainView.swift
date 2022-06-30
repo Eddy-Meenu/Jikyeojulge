0d0af3a11a4eb23505de201018be9439cc989e9f
JUNY0110 <98405970+JUNY0110@users.noreply.github.com>
2022-06-30 23:58:08 +0900

    Feat✨: MainView 데이터 호출
    - MainView 데이터 호출을 위한 코드 수정
    - navigationbarHidden 추가

--- Jikyeojulge/View/MainView.swift
+++ Jikyeojulge/View/MainView.swift
@@ -8,57 +8,74 @@
 import SwiftUI
 
 struct MainView: View {
+    @Environment(\.managedObjectContext) private var viewContext
+    
+    @FetchRequest(entity: PersonalInfoEntity.entity(), sortDescriptors: [
+        NSSortDescriptor(keyPath: \PersonalInfoEntity.id, ascending: true),
+        NSSortDescriptor(keyPath: \PersonalInfoEntity.name, ascending: false),
+        NSSortDescriptor(keyPath: \PersonalInfoEntity.photoImage, ascending: false),
+        NSSortDescriptor(keyPath: \PersonalInfoEntity.bloodType, ascending: false),
+        NSSortDescriptor(keyPath: \PersonalInfoEntity.birth, ascending: false),
+        NSSortDescriptor(keyPath: \PersonalInfoEntity.contact1, ascending: false),
+        NSSortDescriptor(keyPath: \PersonalInfoEntity.contact2, ascending: false)])
+    var personalInfo: FetchedResults<PersonalInfoEntity>
+    
+    @State public var image: Data = .init(count: 0)
+
     @State var medicalRecord: String = "지병에 대해 적어주세요."
     
     var body: some View {
         ZStack {
             Color.mainBlue.ignoresSafeArea()
-            VStack {
-                Image(systemName: "photo")
-                    .resizable()
-                    .clipShape(Circle())
-                    .foregroundColor(.mainBlue)
-                    .scaledToFill()
-                    .frame(width: 150, height: 150)
-                
-                
+            
+            ForEach(personalInfo, id: \.id) { info in
                 VStack {
-                    HStack {
-                        Text("이름")
-                        Spacer()
-                        Text("에디")
-                            
-                    }
+                    Image(uiImage: UIImage(data: info.photoImage ?? self.image)!)
+                        .resizable()
+                        .clipShape(Circle())
+                        .foregroundColor(.mainBlue)
+                        .scaledToFill()
+                        .frame(width: 150, height: 150)
                     
-                    Divider()
                     
-                    HStack {
-                        Text("생년월일")
-                        Spacer()
-                        Text("1999년 01월 01일")
+                    VStack {
+                        HStack {
+                            Text("이름")
+                            Spacer()
+                            Text(info.name ?? "")
+                        }
+                        
+                        Divider()
+                        
+                        HStack {
+                            Text("생년월일")
+                            Spacer()
+                            Text(info.birth ?? "")
+                        }
+                        Divider()
+                        
+                        HStack {
+                            Text("혈액형")
+                            Spacer()
+                            Text(info.bloodType ?? "")
+                        }
+                        Divider()
+                        
+                        HStack {
+                            Text("비상연락처")
+                            Spacer()
+                            Text(info.contact1 ?? "")
+                        }
+                        .padding(.bottom, 2)
+                        HStack {
+                            Spacer()
+                            Text(info.contact2 ?? "")
+                        }
+                        Divider()
                     }
-                    Divider()
-                    
-                    HStack {
-                        Text("혈액형")
-                        Spacer()
-                        Text("A+")
-                    }
-                    Divider()
-                    
-                    HStack {
-                        Text("비상연락처")
-                        Spacer()
-                        Text("010-0000-0000")
-                    }
-                    .padding(.bottom, 2)
-                    HStack {
-                        Spacer()
-                        Text("010-2222-2222")
-                    }
-                    Divider()
-                }
-                .padding(.horizontal, 24)
+                    .padding(.horizontal, 24)
+            
+            
                 
                 HStack {
                     Text("의료 기록")
@@ -68,12 +85,14 @@ struct MainView: View {
                 
                 
                 CustomTextEditor(placholder: "지병에 대해 적어주세요", medicalRecord: $medicalRecord)
+                }
             }
             .frame(width: 350, height: 630)
             .background(RoundedRectangle(cornerRadius: 20)
                 .fill(Color.mainWhite)
                 .shadow(color: .gray.opacity(0.25), radius: 10, x: 2, y: 2))
         }
+        .navigationBarHidden(true)
     }
 }
     
