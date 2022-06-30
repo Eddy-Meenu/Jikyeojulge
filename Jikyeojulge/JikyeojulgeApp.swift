71b95a4eb2384cd409fd14a2cd956e49b1176205
JUNY0110 <98405970+JUNY0110@users.noreply.github.com>
2022-06-30 23:59:20 +0900

    Feat✨: 재실행 시, MainView 호출 조건 추가
    - 초기세팅 후, 재실행 시 MainView로 이동하는 조건 추가

--- Jikyeojulge/JikyeojulgeApp.swift
+++ Jikyeojulge/JikyeojulgeApp.swift
@@ -9,16 +9,22 @@ import SwiftUI
 
 @main
 struct JikyeojulgeApp: App {
+
+    @AppStorage("initSetting") var initSetting: Bool = UserDefaults.standard.bool(forKey: "initSetting")
+    
     let persistenceController = PersistenceController.shared
 
     var body: some Scene {
         WindowGroup {
-            NavigationView {
-                InitSettingView()
+            NavigationView{
+                if initSetting {
+                    MainView()
+                } else {
+                    InitSettingView()
+                }
             }
             .navigationBarHidden(true)
-//            ContentView()
-//                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+            .environment(\.managedObjectContext, persistenceController.container.viewContext)
         }
     }
 }
