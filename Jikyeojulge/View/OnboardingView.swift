90f896811ff1ea9ca1d4f36cb065a20ba606e70a
JUNY0110 <98405970+JUNY0110@users.noreply.github.com>
2022-07-09 01:43:03 +0900

    Feat✨: OnboardingView 구현(임시)
    - 디테일 수정 필요

--- Jikyeojulge/View/OnboardingView.swift
+++ Jikyeojulge/View/OnboardingView.swift
@@ -8,10 +8,44 @@
 import SwiftUI
 
 struct OnboardingView: View {
+
+    let content = OnboardingComponent()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+
+        TabView {
+            ForEach(0..<6) { num in
+                OnboardingPage(title: content.title[num],
+                               description: content.description[num],
+                               imageName: content.imageName[num])
+            }
+        }.tabViewStyle(PageTabViewStyle())
     }
 }
+
+struct OnboardingPage: View {
+
+    let title: String
+    let description: String
+    let imageName: String
+    
+    var content = 0
+    
+    var body: some View {
+     
+        VStack {
+            Text(title)
+                .font(.title)
+            Text(description)
+                .font(.title3)
+            Image(imageName)
+                .resizable()
+                .scaledToFit()
+                .frame(width: 250, height: 541)
+        }
+    }
+}
+
 
 struct OnboardingView_Previews: PreviewProvider {
     static var previews: some View {
