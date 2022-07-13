69b142a388e300f13e88ae6986f94f321f92a869
Juny0110 <wnsdyds403@naver.com>
2022-07-14 00:44:27 +0900

    Feat✨: 진단서 사진 촬영뷰/기능 추가
    - Cancel이 안 되는 버그 잔재

---
+++ Jikyeojulge/Model/PhotoDirectInputEntity+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  PhotoDirectInputEntity+CoreDataClass.swift
+//  Jikyeojulge
+//
+//  Created by 지준용 on 2022/07/13.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(PhotoDirectInputEntity)
+public class PhotoDirectInputEntity: NSManagedObject {
+
+}
